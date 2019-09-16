23f183939633cf17897d8c50c5774c79b3986b1e
arthur <arthurcarvalho24@gmail.com>
2019-09-16 18:07:53 -0300

    rotas

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const controller = require("../controllers/DiagramController")
 
-router.get("/listar", controller.listar)
+router.get("/listar", controller.listar);
 
-router.get("/buscar/:id", controller.buscar)
+router.get("/buscar/:id", controller.buscar);
 
-router.post("/inserir", controller.inserir)
+router.post("/inserir", controller.inserir);
 
-router.put("/atualizar/:id", controller.atualizar)
+router.put("/atualizar/:id", controller.atualizar);
 
+router.delete("/remover/:id", controller.remover);
 module.exports = router
